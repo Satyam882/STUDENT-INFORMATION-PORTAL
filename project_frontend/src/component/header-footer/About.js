54bb4f2d5ef6a4ff070f6b069e9917d8baa9bb16
Satyam882 <satyampandey882@gmail.com>
2023-03-10 18:17:30 +0530

    new file

---
+++ project_frontend/src/component/header-footer/About.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import  '../style.css';
+
+export const About = () => {
+    return (
+        <div className='app'>
+        <div  className='Container'>
+            <h1 align='center' >Welcome to IACSD</h1>
+            <p>
+        
+            IACSD Akurdi is an Authorized Training Centre of C-DAC ACTS Centre for Development of Advanced Computing (C-DAC)
+             is the premier R&D organization of the Ministry of Electronics and Information Technology (MeitY)
+              for carrying out R&D in IT, Electronics and associated areas. Different areas of C-DAC, 
+              had originated at different times, many of which came out as a result of identification of opportunities.</p>
+
+               <p> 
+               The setting up of C-DAC in 1988 itself was to built Supercomputers in context of denial of import of Supercomputers by USA.
+                Since then C-DAC has been undertaking building of multiple generations of Supercomputer starting from PARAM with 1 GF in 1988.</p>
+
+            <p> Almost at the same time, C-DAC started building Indian Language Computing Solutions with setting up of GIST group 
+                (Graphics and Intelligence based Script Technology); National Centre for Software Technology (NCST) 
+                set up in 1985 had also initiated work in Indian Language Computing around the same period.
+                Electronic Research and Development Centre of India (ER&DCI) with various constituents starting as adjunct entities of
+                 various State Electronic Corporations, had been brought under the hold of Department of Electronics and Telecommunications 
+                 (now MeitY) in around 1988. They were focusing on various aspects of applied electronics, technology and applications.
+                </p>
+
+            
+                   
+            
+        </div>
+        </div>
+    )
+}
---
+++ project_frontend/src/component/header-footer/fee.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export const Fee = () => {
+    return ( 
+       <div className='contact'>
+       <h1 align='center'>CDAC FEE FOR PG_DAC</h1>
+      <br></br>
+      <table className="table">
+    
+  <tbody>
+     
+    <tr>
+      <th scope="row">1</th>
+      <td>Seat Booking Fee</td>
+      <td>RS. 11500</td>
+     
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Admission Fee</td>
+      <td>Rs. 94000</td>
+     
+    </tr>
+  </tbody>
+</table>
+       </div>
+       
+       
+    )
+}
